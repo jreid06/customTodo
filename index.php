3359bfe8443da61696c553cbd947b298cb585e84
Jason Reid <jasonbrandon9406@gmail.com>
2017-07-07 00:32:50 +0100

    git ignore added

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+
+  <meta name="description" content="">
+  <meta name="keywords" content="portfolio, website, projects, clients, jason brandon reid, jbr">
+  <meta name="author" content="Jason Brandon Reid">
+
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- <link href="https://fonts.googleapis.com/css?family=Quicksand|Raleway:400,600" rel="stylesheet"> -->
+  <link href="https://fonts.googleapis.com/css?family=Boogaloo|Fredericka+the+Great" rel="stylesheet">
+
+  <!-- Custom CSS -->
+  <!-- <link rel="stylesheet" href="css/hover-min.css"> -->
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/todo.css">
+
+
+  <title>TEMPLATE</title>
+</head>
+
+<body>
+
+  <!-- <div class="page1"> -->
+    <div class="container-fluid home-screen">
+      <div class="content">
+              <!-- <p class="storage s1" id="storage1"></p>
+              <p class="storage s2" id="storage2"></p>
+              <button class="s4" id="myBtn2">update Value with Vauxhall</button>
+              <button class="s3" id="myBtn">update Value with Porshe</button>
+              <button class="s5" id="myBtn3">Create</button>
+              <div class="showCreated" id="screate"> -->
+                <!-- <p class="created"></p> -->
+              </div>
+              <section class="title">
+                <div class="titles">
+                  <p class="h2-cus" data-fade="true" data-fade-delay="0s">my <span class="underline"></span></p>
+                  <p class="h2-cus" data-fade="true" data-fade-delay="1.5s">todo <span class="underline"></span></p>
+                  <p class="h1-cus" data-fade="true" data-fade-delay="3s">list <span class="underline"></span></p>
+                </div>
+              </section>
+              <section class="home-buttons">
+                <div class="create" style="position: relative; z-index: 1000000;">
+                    <a href="categories.html">create</a>
+                </div>
+
+                <button class="load" id="load">load</button>
+              </section>
+
+              <footer class="home-footer">
+                <section class="social-media">
+                  <ul>
+                    <li><span class="fa fa-facebook hvr-float"></span></li>
+                    <li><span class="fa fa-instagram"></span></li>
+                    <li><span class="fa fa-linkedin"></span></li>
+                    <li><span class="fa fa-google"></span></li>
+                  </ul>
+                  <p style="width:100%; text-align:center"><a href="http://www.jbreid.co.uk" style="color: #fff">designed &amp; developed by jbreid</a></p>
+                </section>
+              </footer>
+      </div>
+
+    </div>
+  <!-- </div> -->
+
+  <!-- <div class="page2"> -->
+    <div class="container-fluid create-category">
+      <div class="content">
+        <section class="categories-header">
+          <h1>categories</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+        </section>
+
+        <section class="create-info">
+          <div class="crteBtn cat-create" id="catCreate">
+            <div class="background_slide" id="bs01"></div>
+            <div class="icon">
+              <span class="fa fa-plus"></span>
+            </div>
+            <p><span>CREATE CATEGORY</span></p>
+          </div>
+          <div class="crteBtn cat-list" id="listCreate">
+            <div class="background_slide" id="bs02"></div>
+            <div class="icon">
+              <span class="fa fa-plus"></span>
+            </div>
+            <p><span>CREATE LIST</span></p>
+          </div>
+
+          <div class="smallButtons more">
+            <div class="sbtn allCat" data-toggle="modal">
+              <p>all categories</p>
+            </div>
+
+            <div class="sbtn unCat" data-toggle="modal">
+              <p>un-categorised items</p>
+            </div>
+          </div>
+        </section>
+
+        <footer>
+          <section class="social-media">
+            <ul>
+              <li><span class="fa fa-facebook"></span></li>
+              <li><span class="fa fa-instagram"></span></li>
+              <li><span class="fa fa-linkedin"></span></li>
+              <li><span class="fa fa-google"></span></li>
+            </ul>
+            <p style="width:100%; text-align:center"><a href="http://www.jbreid.co.uk" target="_blank" style="color: #fff">designed &amp; developed by jbreid</a></p>
+          </section>
+        </footer>
+      </div>
+
+    </div>
+  <!-- </div> -->
+
+
+
+
+
+
+  <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <!--font awsome-->
+  <script src="https://use.fontawesome.com/7c0f3e2f67.js"></script>
+  <!-- <script src="js/velocity.js"></script> -->
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="js/velocity.js"></script>
+  <script src="js/wow.min.js"></script>
+  <script>
+    new WOW().init();
+  </script>
+  <script src="js/todo.js"></script>
+</body>
+
+</html>
