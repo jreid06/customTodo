2bb4439572ae82d65072a7859d8892783f54185a
Jason Reid <jasonbrandon9406@gmail.com>
2017-07-29 20:37:36 +0100

    category lists are save and loaded when modal is triggered

--- js/todo3.js
+++ js/todo3.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const catArray = (localStorage.getItem('categories')) ? JSON.parse(localStorage.getItem('categories')):[];
+const catArray = (localStorage.getItem('categories')) ? JSON.parse(localStorage.getItem('categories')) : [];
 
 // create out count function
 const count = (function() {
@@ -24,15 +24,19 @@ const count = (function() {
 // create storage for all categories
 const categories = (function(input) {
     let name = '';
+
     const storeCat = (val) => {
         catArray.push(val);
         catArray[catArray.length - 1].genID();
         console.log(catArray);
         if (catArray.length > 0) {
-          $('.cat-input').animate({
-            marginTop: '10%'
-          }, 600);
+            $('.cat-input').animate({
+                marginTop: '10%'
+            }, 600);
         }
+    }
+    const addTodo = (val, pos) => {
+        catArray[pos].todo.push(val);
     }
 
     function createCat(input) {
@@ -62,7 +66,13 @@ const categories = (function(input) {
         add: function() {
             createCat(name);
             (function() {
-              localStorage.setItem('categories', JSON.stringify(catArray));
+                localStorage.setItem('categories', JSON.stringify(catArray));
+            })();
+        },
+        todoFunc: function(pos) {
+            addTodo(name, pos);
+            (function() {
+                localStorage.setItem('categories', JSON.stringify(catArray));
             })();
         },
         storedVal: function() {
@@ -73,82 +83,139 @@ const categories = (function(input) {
 
 
 $(document).ready(function() {
-    const render = ()=>{
-      console.log('render function works');
-      console.log(catArray);
-      setTimeout(function(){
-        for (var i = 0; i < catArray.length; i++) {
-          count.add();
-          createCatList('cat',catArray[i].name,'#catList_cards');
-        }
 
-        if (catArray.length > 0) {
-          $('.cat-input').animate({
-            marginTop: '10%'
-          }, 700);
-        }
-      }, 1000);
+    let mdlID = '';
+    const render = () => {
+        console.log('render function works');
+        console.log(catArray);
+        setTimeout(function() {
+            for (var i = 0; i < catArray.length; i++) {
+                count.add();
+                createCatList('cat', catArray[i].name, '#catList_cards');
+            }
+
+            if (catArray.length > 0) {
+                $('.cat-input').animate({
+                    marginTop: '10%'
+                }, 700);
+            }
+        }, 1000);
 
     }
 
-    // save all categories to local storage
-    function categoriesArrayUpdated() {
-        localStorage.setItem('categories', JSON.stringify(catArray));
+    const renderListItems = (val) => {
+        console.log('render function works');
+        console.log(catArray);
+        setTimeout(function() {
+            // console.log(`Val: ${val}`);
+            let len = catArray[val].todo.length;
+            for (var i = 0; i < len; i++) {
+              console.log(`i: ${i}`);
+              console.log(catArray[val].todo[i]);
+              createListItem(catArray[val].todo[i], 'todoList');
+            }
+            // for (var i = 0; i <= catArray[val].todo.length; i++) {
+            //     createListItem(catArray[val].todo[i], 'todoList');
+            // }
+        }, 1000);
+
     }
 
+    $('#catmodal').on('show.bs.modal', function(event) {
+        console.log($(event.relatedTarget));
+        let $button = $(event.relatedTarget),
+            $btnParent = $button.parents('li').eq(0).attr('data-id-count'),
+            modal = $(this),
+            catTitle = catArray[$btnParent - 1].name;
+
+        console.log($btnParent);
+        mdlID = $btnParent;
+
+
+        modal.find('.modal-title').html(catTitle);
+        if (catArray[$btnParent - 1].todo.length > 0) {
+          renderListItems($btnParent - 1);
+        }
+        else {
+          console.log('renderListItems doesnt run ');
+        }
+
+
+
+    })
+
+    $('#catmodal').on('hidden.bs.modal', function() {
+        let modal = $(this);
+        modal.find('.modal-title').html('');
+        modal.find('#todoList').html('');
+    });
 
 
     // click events
-    $('#inpCat').on('keypress', function(event) {
-        let button = $(this);
-        if (event.keyCode === 13) {
-            console.log('Enter event has been run');
-            console.log('-------------------------');
-            console.log(button);
-            let type = $(button).attr('data-type'),
-                inpVal = $(button).val();
+    const inputFunc = (inp, add, clear) => {
+        $(inp).on('keypress', function(event) {
 
+            let button = $(this);
+            if (event.keyCode === 13) {
+                console.log('enter hit');
+                console.log('Enter event has been run');
+                console.log('-------------------------');
+                console.log(button);
+                let type = $(button).attr('data-type'),
+                    inpVal = $(button).val();
+
+                console.log(type);
+                console.log(inpVal);
+                console.log('');
+                console.log('');
+                if (type === 'cat') {
+                    categories.getInputVal(inpVal);
+                    categories.add();
+                    createCatList(type, inpVal, '#catList_cards');
+                    // checkArrayLength();
+                    // CatListFunctions();
+                } else {
+                    console.log('list click events should be here');
+                    console.log(`MDL-ID ${mdlID}`);
+                    categories.getInputVal(inpVal);
+                    categories.todoFunc(mdlID - 1);
+                    createListItem(inpVal, 'todoList');
+
+                }
+                button.val('');
+            }
+        })
+
+        $(add).on('click', function(event) {
+            console.log('clicked');
+            // get the button that triggered the event & its type e.g cat or list
+            let button = `#${$(event.target).parent().attr('id')}`,
+                type = $(button).attr('data-type'),
+                inputBox = `#${$(event.target).parents().eq(1).children(0).attr('id')}`,
+                inpVal = $(inputBox).val();
+
+            console.log('input id\'s, data-type attrib and VAl');
+            console.log('----------------------------');
+            console.log(button);
             console.log(type);
+            console.log(inputBox);
             console.log(inpVal);
-            console.log('');
-            console.log('');
+
+            console.log('---------------');
             if (type === 'cat') {
                 categories.getInputVal(inpVal);
-                categories.add('');
+                categories.add();
                 createCatList(type, inpVal, '#catList_cards');
                 // checkArrayLength();
-                // CatListFunctions();
+
+            } else {
+                console.log('list click events should be here');
             }
-            button.val('');
-        }
-    })
+            $(inputBox).val('');
 
-    $('#addCat').on('click', function(event) {
+        });
+    }
 
-        // get the button that triggered the event & its type e.g cat or list
-        let button = `#${$(event.target).parent().attr('id')}`,
-            type = $(button).attr('data-type'),
-            inputBox = `#${$(event.target).parents().eq(1).children(0).attr('id')}`,
-            inpVal = $(inputBox).val();
-
-        console.log('input id\'s, data-type attrib and VAl');
-        console.log('----------------------------');
-        console.log(button);
-        console.log(type);
-        console.log(inputBox);
-        console.log(inpVal);
-
-        console.log('---------------');
-        if (type === 'cat') {
-            categories.getInputVal(inpVal);
-            categories.add('');
-            createCatList(type, inpVal, '#catList_cards');
-            // checkArrayLength();
-
-        }
-        $(inputBox).val('');
-
-    });
 
     const createCatList = (type, val, listDest) => {
 
@@ -220,13 +287,51 @@ $(document).ready(function() {
 
             }
 
-          if (catArray.length === 0) {
-            $('.cat-input').animate({
-              marginTop: '40%'
-            }, 600);
-          }
+            if (catArray.length === 0) {
+                $('.cat-input').animate({
+                    marginTop: '40%'
+                }, 600);
+            }
 
         })
+    }
+
+    const createListItem = (itemTitle, todoListID) => {
+
+        console.log('list created');
+        let listItem = `
+          <li class="list-card animated fadeIn" id="listItm">
+                <div class="bar">
+                  <div class="row description">
+                    <div class="col-xs-12">
+                      <section class="custom-height">
+                        <p class="category-t">${itemTitle}</p>
+                        <p class="delete-cat delete-list" id="delItem"><i class="fa fa-times-circle-o hvr hvr-grow"></i></p>
+                      </section>
+                    </div>
+                  </div>
+                  <div class="row bar">
+                      <div class="col-xs-2 col-xs-offset-3 col active">
+                        <div class="list-buttons" id="lb1">
+                          <span class="fa fa-tasks"></span>
+                        </div>
+                      </div>
+                      <div class="col-xs-2 col">
+                        <div class="list-buttons" id="lb2">
+                          <span class="fa fa-spinner"></span>
+                        </div>
+                      </div>
+                      <div class="col-xs-2 col">
+                        <div class="list-buttons" id="lb3">
+                        <span class="fa fa-check"></span>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+              </li>`;
+
+        //todolist variable is the id of the list to append the item to
+        $(`#${todoListID}`).prepend(listItem);
     }
 
     const updateDataIdAttr = (position) => {
@@ -258,8 +363,6 @@ $(document).ready(function() {
 
             // update the delete button id
             $(`#${newList.listId}`).find('p').eq(1).children().attr('id', `delCat-${newList.id}`);
-
-
         }
 
     }
@@ -267,7 +370,11 @@ $(document).ready(function() {
 
     render();
 
+    // category page input boxes controls
+    inputFunc('#inpBox', '#addItem');
 
+    // modal page input boxes controls
+    inputFunc('#inpList', '#addList');
 
 
 
